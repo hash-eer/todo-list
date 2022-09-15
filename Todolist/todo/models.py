5ba9afd2c5463a2985e5479777c72f8fcee8c832
Hasheer <mohammedhasheer49@gmail.com>
2022-09-15 14:45:37 +0530

    todo list app

---
+++ Todolist/todo/models.py
@@ -0,0 +1,17 @@
+from ast import Delete
+from django.db import models
+#from django.contrib.auth.models import User #this helps us to manage everything of the user
+# Create your models here.
+
+class task(models.Model):
+    
+    title=models.CharField(max_length=200 , null=True)#title
+   
+    complete =models.BooleanField(default=False) #this is a checkbox it is set to false because initially task should not complete 
+    created =models.DateTimeField(auto_now_add=True) #the parameter passes automatically adds the time to task
+    
+    def __str__(self):
+        return self.title #returns only the name of the title when an object is created
+    
+    
+        
---
+++ Todolist/todo/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+# Create your views here.
+
+def home(request):
+    tasks= task.objects.all()
+    form = TaskForm()
+    
+    if request.method == 'POST':
+        form=TaskForm(request.POST)
+        
+        if form.is_valid():
+            form.save() #this will save to data base
+            
+        return redirect('/')
+        
+    context={'tasks':tasks ,'form':form}
+    return render(request,'todo/list.html',context)
+
+
+def updateTask(request,pk):
+    task1 =task.objects.get(id=pk)
+    
+    form=TaskForm(instance=task1)
+    
+    if request.method=='POST':
+        form=TaskForm(request.POST,instance=task1)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/')
+        
+    
+    context ={'form':form}
+    return render(request,'todo/update_task.html',context)
+
+def deleteTask(request,pk):
+    item=task.objects.get(id=pk)
+    
+    
+    if request.method=='POST':
+        item.delete()
+        return redirect('/')
+    
+    context={'item':item}
+    return render(request,'todo/delete.html',context)
+    
